98eddd2454a20e544b1e73e28a02e86c45f0d77a
jiwon <wldnjs4685@gmail.com>
2024-09-28 20:47:58 +0900

    GenItitial Set

--- Association Rules Mining.py
+++ Association Rules Mining.py
@@ -1,12 +1,31 @@
 import csv
+from collections import defaultdict # 키 없이 사용 가능한 dictionary
 
 
 
 class Aprior:
 
-
     def __init__(self, filename, threshold):
+        self.filename = filename
         self.threshold = threshold
-        self.dataset = self.ReadData(filename)
+        self.frequentItemSet = {}
 
-    def ReadData():
+    # 첫 dataset 만들기
+    def GenInitialSet(self):
+        itemset = defaultdict(int)
+        with open(self.filename, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader: 
+                transaction = set(row)
+                for item in transaction:
+                    itemset[frozenset([item])] +=1
+
+        # frequent를 세서 dic 만들기
+        frequentItemSet = {itemset:count for itemset, count in itemset.items() if count >= self.threshold}
+            
+        return frequentItemSet
+    
+
+a = Aprior('market.csv',threshold=3 )
+f = a.GenInitialSet()
+print(f)
